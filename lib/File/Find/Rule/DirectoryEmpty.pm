56fa53ee03957b81459686cdd37a3386d1553ca7
Leo Charre <leocharre@cpan.org>
2008-05-17 16:39:06 +0000

    Import of LEOCHARRE/File-Find-Rule-DirectoryEmpty-1.10 from CPAN.
    gitpan-cpan-distribution: File-Find-Rule-DirectoryEmpty
    gitpan-cpan-version:      1.10
    gitpan-cpan-path:         LEOCHARRE/File-Find-Rule-DirectoryEmpty-1.10.tar.gz
    gitpan-cpan-author:       LEOCHARRE
    gitpan-cpan-maturity:     released

--- lib/File/Find/Rule/DirectoryEmpty.pm
+++ lib/File/Find/Rule/DirectoryEmpty.pm
@@ -2,8 +2,7 @@ package File::Find::Rule::DirectoryEmpty;
 use strict;
 use base 'File::Find::Rule';
 use vars qw($VERSION);
-$VERSION = sprintf "%d.%02d", q$Revision: 1.6 $ =~ /(\d+)/g;
-
+$VERSION = sprintf "%d.%02d", q$Revision: 1.10 $ =~ /(\d+)/g;
 
 sub File::Find::Rule::directoryempty {
 	my $self = shift->_force_object;
@@ -25,7 +24,6 @@ sub File::Find::Rule::directoryempty {
 	);		
 }
 
-	
 
 
 1;
@@ -42,17 +40,33 @@ File::Find::Rule::DirectoryEmpty - find empty directories recursively
 
 	use File::Find::Rule::DirectoryEmpty;
 	
-	my @emptydirs = File::Find::Rule::DirectoryEmpty->directoryempty->in('/home/myself');
+	my @emptydirs = File::Find::Rule->directoryempty->in('/home/myself');
 
-=head1 directoryempty()
+   # another way..
+   
+   my $o = new File::Find::Rule;
+   $o->directoryempty;
+   my @emptydirs = $o->in( $ENV{HOME} );
 
-returns boolean
+=head1 Matching Rules
+
+=head2 directoryempty()
+
+Matches only if it is an empty directory.
 
 =head1 DESCRIPTION
 
+This module inherits File::Find::Rule. It lets you find empty directories recursively.
+Note that a directory with an empty directory inside it is not an empty directory.
+
+=head2 NOTES
+
 Instead of reading full count of directory contents, we return false as soon as we match
-something other then . or ..
-This may not work on windoze.
+something other then . or .. This helps with speed.
+
+=head1 CAVEATS
+
+This may not work on windows platforms. You're welcome to send in a patch for it. 
 
 =head1 AUTHOR
 
